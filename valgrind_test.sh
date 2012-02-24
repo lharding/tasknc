2deaefdb8e9a25833f3f4d15aaf6fd6abbdad362
mjheagle <mjheagle8@gmail.com>
2012-02-23 20:24:02 -0500

    added script to check for memory problems

---
+++ valgrind_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+errorfile="valgrind_errors"
+run="./tasknc"
+valgrind_options="--tool=memcheck --leak-check=full -v --show-reachable=yes"
+
+if [ -e $errorfile ]; then
+    rm $errorfile
+fi
+
+valgrind $valgrind_options --log-file="$errorfile" "$run" 
+vim "$errorfile"
