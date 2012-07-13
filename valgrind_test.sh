cdc0bf8be0ec8a2e92fe3d849f55a450af981375
mjheagle <mjheagle8@gmail.com>
2012-07-13 11:50:14 +0000

    valgrind suppressions

--- valgrind_test.sh
+++ valgrind_test.sh
@@ -2,7 +2,7 @@
 
 errorfile="valgrind_errors"
 run="./tasknc"
-valgrind_options="--tool=memcheck --leak-check=full -v --show-reachable=yes --track-origins=yes"
+valgrind_options="--tool=memcheck --leak-check=full -v --show-reachable=yes --track-origins=yes --suppressions=valgrind.supp"
 
 if [ -e $errorfile ]; then
     rm $errorfile
