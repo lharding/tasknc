6d23f15924311cf03eac384863891bea2b269028
mjheagle <mjheagle8@gmail.com>
2013-02-02 12:20:02 -0600

    include cursutil in tasklist.c

--- src/tasklist.c
+++ src/tasklist.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "tasklist.h"
+#include "cursutil.h"
 
 /* temporary function to test printing tasks */
 int simple_print_task(WINDOW *win, const int line, const int width, struct task * task) {
