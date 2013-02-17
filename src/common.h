a0f0880cad67e39e3ad172ba6811bec609be5912
mjheagle <mjheagle8@gmail.com>
2013-02-17 13:40:59 -0600

    define a variable struct in common.h

--- src/common.h
+++ src/common.h
@@ -7,7 +7,20 @@
 #ifndef _TASKNC_COMMON_H
 #define _TASKNC_COMMON_H
 
+/* conditional free's */
 #define check_free(a) { if (a != NULL) free(a); }
 #define check_fclose(a) { if (a != NULL) fclose(a); }
+
+/* variable definition */
+enum var_type { VAR_UNKNOWN, VAR_STRING, VAR_INT, VAR_FLOAT, VAR_CHAR };
+struct var {
+        enum var_type type;
+        union {
+                char c;
+                int d;
+                float f;
+                char *s;
+        } value;
+};
 
 #endif
